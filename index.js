0800c7cb75e6d71df252f401ded2bf535b28e849
Michael Kirschbaum <mkirschbaum@car.fit>
2017-06-29 14:13:48 -0700

    change template engine

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
