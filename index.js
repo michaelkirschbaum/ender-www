2fe46b25c74de40a0913ef44c552eacd487449ea
Michael Kirschbaum <mkirschbaum@car.fit>
2017-06-29 21:47:32 -0700

    add dependency

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var psg = require('pg-promise')
+// var db = psg('')
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
